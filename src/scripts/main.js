07ec78249148446aa8e7736546271e3b18569521
cgm616 <cgm616@me.com>
2020-03-25 11:28:38 -0400

    Finish basic functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,15 @@
+import renderMathInElement from "katex/dist/contrib/auto-render";
+
+const baseUrl = "http://localhost:8080/public/"
+
 document.addEventListener('DOMContentLoaded', () => {
+
+    const url = new URL(window.location.href);
+    const pid = url.searchParams.get("id");
+
+    if (pid != null) {
+        showPaste(pid);
+    }
 
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
@@ -22,4 +33,62 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+
+    const goButton = document.getElementById("go-to-pastex-id");
+    const pastexId = document.getElementById("pastex-id");
+
+    goButton.addEventListener('click', () => {
+        // TODO: make this use query string
+        // window.location.href = window.location.href + pastexId.value;
+    })
+
+    const shareButton = document.getElementById("share-button");
+    const shareText = document.getElementById("share-text");
+
+    shareButton.addEventListener('click', () => {
+        var s = new String(shareText.value);
+
+        fetch('https://cors-anywhere.herokuapp.com/http://ix.io', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({ "f:1": s, }),
+        }).then((data) => data.text())
+            .then((text) => goToId(text.replace("http://ix.io/", "")));
+    })
+
 });
+
+function goToId(id) {
+    window.location.href = baseUrl + "?id=" + id;
+}
+
+function showPaste(id) {
+    const home = document.getElementById('home');
+    const paste = document.getElementById('paste');
+    toggleHidden(home);
+    toggleHidden(paste);
+
+
+    document.getElementById('pasteid').append(id);
+
+    const pasteContent = document.getElementById('paste-content');
+
+    fetch('https://cors-anywhere.herokuapp.com/http://ix.io/' + id, {
+        method: 'GET'
+    })
+        .then((data) => data.text())
+        .then((text) => {
+            var node = document.createElement("p");
+            node.append(document.createTextNode(text));
+            pasteContent.append(node);
+            renderMathInElement(pasteContent);
+        });
+}
+
+function toggleHidden(element) {
+    element.classList.toggle("is-hidden-tablet");
+    element.classList.toggle("is-hidden-mobile");
+}
